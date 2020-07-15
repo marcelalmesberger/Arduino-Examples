b4e3b6239e94f3aae90278b13100b6cb2d7493bb
Marcel Almesberger <46678293+marcelalmesberger@users.noreply.github.com>
2020-07-15 20:09:19 +0200

    Add files via upload

---
+++ 04SerialOutput/04SerialOutput.ino
@@ -0,0 +1,10 @@
+// Example 04 Testing Serial Output
+
+void setup() {                   
+  Serial.begin(9600);              // set up Serial library at 9600 bps
+  Serial.println("Hello world!");  // prints hello with ending line break 
+}
+
+void loop(){                       
+                                  // do nothing!
+}
