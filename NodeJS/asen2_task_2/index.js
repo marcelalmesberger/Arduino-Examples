b60ce5893877d7f11d4f7452116549d1eb53c06a
Marcel Almesberger <46678293+marcelalmesberger@users.noreply.github.com>
2020-07-15 20:00:54 +0200

    Delete index.js

--- NodeJS/asen2_task_2/index.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((request, response) => {
-    console.log("Server was called", request);
-    response.end("Hello World!");
-});
-
-server.listen(8080);
